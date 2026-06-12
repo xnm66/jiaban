5f1c97bd0822c5fcbadcd9c70ce28661064a02ab
小柠檬 <54431854+xnm66@users.noreply.github.com>
2026-06-13 00:46:24 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+// sw.js - Service Worker 离线缓存
+const CACHE_NAME = 'jiaban-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './lunar.js',
+    './tiaoxiu.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
